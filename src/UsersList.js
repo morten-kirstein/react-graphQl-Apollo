5a6eb64fe8eadd80a410f02cc15a24f31f104184
Morten Kirstein <info@mortenkirstein.dk>
2020-05-19 12:22:02 +0200

    refactor

--- src/UsersList.js
+++ src/UsersList.js
@@ -4,9 +4,11 @@ export function UsersList({ users, deleteUser }) {
 
 
     const deleteUserClicked = (user) => {
-
-
         deleteUser(user);
+    }
+
+    const isUserInactive = status => {
+        return status === 'Inactive';
     }
 
 
@@ -16,21 +18,12 @@ export function UsersList({ users, deleteUser }) {
             {users.map(user =>
                 <li key={user.id}>
 
-
-
-
-
-
-
-
                     <button onClick={deleteUserClicked}>x</button>
 
+                    {/* Example of inline style conditional witout css ClassName reference */}
                     <pre style={{
-                        textDecoration: user.status === 'Inactive' ? 'line-through' : 'none'
+                        textDecoration: isUserInactive(user.status) ? 'line-through' : 'none'
                     }}>{user.name}</pre>
-
-
-
 
 
                 </li>)}
